24126399b3b751d1a0b5b853c6e338d3ef4d0dc3
SergioBuzi <sergio.buzi97@gmail.com>
2024-02-21 18:17:53 +0100

    style: show

--- resources/views/Project/show.blade.php
+++ resources/views/Project/show.blade.php
@@ -1,17 +1,37 @@
 @extends('layouts.app')
 @section('content')
-    <h1 class="text-center my-3">Project [{{ $project->id }}]Detail:</h1>
-    <div class="detail">
-        <div><b>Description: </b>{{ $project->description }}</div>
-        <a href="{{ $project->link }}">Project Link</a>
+    <h1 class="text-center my-3">Project [{{ $project->id }} ]Details:</h1>
+
+    <div class="container d-flex">
+        
+        <div class="d-flex card justify-content-center flex-row">
+
+            <div class="d-flex">
+                <img src="{{$project->image}}" alt="">
+            </div>
+
+            <div class="items-cont m-5">
+                <h3><b>Nome del progetto: </b> {{$project->name}}</h3>
+                <p><h3>Description: </h3>{{ $project->description }}</p>
+                <h3><b>Tecnologia utilizzata: </b> {{$project->tecnology}}</h3>
+                <a href="{{ $project->link }}">Link GitHub</a>
+            </div>
+        </div>
     </div>
 @endsection
 
 <style>
-    .detail {
-        border: 1px solid black;
-        width: 600px;
-        margin: 0 auto;
-        padding: 5px;
+    body{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    img{
+        width: 500px
+    }
+
+    p{
+        font-size: 20px
     }
 </style>
