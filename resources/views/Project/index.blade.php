5e64b8c3bf9acc8732cb9ecf8d94f33c221d816a
SergioBuzi <sergio.buzi97@gmail.com>
2024-02-21 13:15:15 +0100

    config: index

--- resources/views/Project/index.blade.php
+++ resources/views/Project/index.blade.php
@@ -1,29 +1,32 @@
 @extends('layouts.app')
 @section('content')
+
     <h1 class="text-center mb-5">
         Projects
     </h1>
+
     <ul class="d-flex flex-wrap justify-content-center">
         @foreach ($projects as $project)
+
             <li class=" m-3 project">
+
                 <img class="mb-3" src="{{ $project->image }}" alt="" style="width: 240px">
                 <h3 class="mb-2 p-2"> <b>Title: </b>{{ $project->name }}</h3>
-
                 <div class="tecnology p-2"><b>Technologies: </b>{{ $project->tecnology }}</div>
+                
             </li>
+
         @endforeach
     </ul>
+
 @endsection
 
 
-<style lang="scss">
+<style>
     .project {
-
         border: 1px solid lightgray;
         width: 240px;
         border-radius: 8px;
-
-
     }
 
     img {
