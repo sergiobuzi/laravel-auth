9960ccd310f2ca3923b0841e53fda34983477ef5
SergioBuzi <sergio.buzi97@gmail.com>
2024-02-21 12:08:50 +0100

    view: add content in view

--- resources/views/Project/index.blade.php
+++ resources/views/Project/index.blade.php
@@ -2,5 +2,15 @@
 @section('content')
 <h1>
     hello aurora
+
+    <ul>
+        @foreach ($projects as $project)
+        <li>
+            <img src="{{$project -> image}}" alt="">
+            {{$project -> name}}
+            {{$project -> tecnology}}
+        </li>
+        @endforeach
+    </ul>
 </h1>
 @endsection
