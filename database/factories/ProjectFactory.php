655003f8f31c879184acaed4c503bd8be9504af6
SergioBuzi <sergio.buzi97@gmail.com>
2024-02-21 11:38:55 +0100

    config: add migrations

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake() ->words(3, true),
+            'description'=> fake() ->paragraph(),
+            'image'=> fake() ->imageUrl(640, 480, 'animals', true),
+            'tecnology'=> fake() ->words(5, true),
+            'link'=> fake() ->url(),
+        ];
+    }
+}
